c65ab2ff1d2a5326efa3680f6a58f4c3e49fca72
getFrog <154066750+getFrog@users.noreply.github.com>
2024-10-16 22:02:00 +0200

    Update turing.jsx

--- src/pages/turing.jsx
+++ src/pages/turing.jsx
@@ -3,18 +3,11 @@ import { animated } from "react-spring";
 import { useWiggle } from "../hooks/wiggle";
 
 
-
-
 export default function Turing() {
   return (
     <>
       <div>
         <h1>hello</h1>
-      </div>
-    </>
-  );
-}
-</h1>
       </div>
     </>
   );
