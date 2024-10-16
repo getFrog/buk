d66a8edbc5f8780237cb83fd0a317f1b623c639b
Glitch (glitch-hello-react) <none>
2024-10-16 19:11:31 +0000

    🎯🐧 Checkpoint ./package.json:24750864/1464 ./src/pages/turing.jsx:24750864/737

---
+++ src/pages/turing.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { animated } from "react-spring";
+import { useWiggle } from "../hooks/wiggle";
+import { Link } from "wouter";
+
+export default function Turing() {
+  return;
+  <>
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+<TextField id="filled-basic" label="Filled" variant="filled" />
+<TextField id="standard-basic" label="Standard" variant="standard" />
+  </>;
+}
