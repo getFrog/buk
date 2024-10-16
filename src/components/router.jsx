1b778a1845ccf647663f73a2f76b8fe0bd2f8dca
Glitch (hot-fierce-vault) <none>
2024-10-16 19:27:32 +0000

    🎀👩‍👩‍👦 Checkpoint ./src/pages/turing.jsx:24750864/1253 ./src/components/router.jsx:24750864/1319

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -2,18 +2,21 @@ import * as React from "react";
 import { Switch, Route, Router } from "wouter";
 import Home from "../pages/home";
 import About from "../pages/about";
+import Turing from "../pages/turing";
+
 
 /**
-* The router is imported in app.jsx
-*
-* Our site just has two routes in it–Home and About
-* Each one is defined as a component in /pages
-* We use Switch to only render one route at a time https://github.com/molefrog/wouter#switch-
-*/
+ * The router is imported in app.jsx
+ *
+ * Our site just has two routes in it–Home and About
+ * Each one is defined as a component in /pages
+ * We use Switch to only render one route at a time https://github.com/molefrog/wouter#switch-
+ */
 
 export default () => (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </Switch>
+  <Switch>
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/turing" component={Turing} />
+  </Switch>
 );
--- src/pages/turing.jsx
+++ src/pages/turing.jsx
@@ -2,14 +2,13 @@ import * as React from "react";
 import { animated } from "react-spring";
 import { useWiggle } from "../hooks/wiggle";
 import { Link } from "wouter";
-import { Textfield } from "mui/material"
-import * from '@mui/material';
+
+//import { TextField } from "@mui/material";
 
 export default function Turing() {
   return;
   <>
-    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-<TextField id="filled-basic" label="Filled" variant="filled" />
-<TextField id="standard-basic" label="Standard" variant="standard" />
+    <div></div>  hello
+
   </>;
 }
