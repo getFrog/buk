d8af74460b3ff9860695ff83d14ffc4e020b395c
Glitch (hot-fierce-vault) <none>
2024-10-16 19:38:42 +0000

    👽🌱 Checkpoint ./src/pages/turing.jsx:24750864/3835 ./src/pages/home.jsx:24750864/28

--- src/pages/turing.jsx
+++ src/pages/turing.jsx
@@ -2,13 +2,25 @@ import * as React from "react";
 import { animated } from "react-spring";
 import { useWiggle } from "../hooks/wiggle";
 import { Link } from "wouter";
-
-//import { TextField } from "@mui/material";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 export default function Turing() {
-  return;
-  <>
-    <div></div>  hello
+  return (
+    <>
+      <Box
+        component="form"
+        sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Multiline"
+          multiline
 
-  </>;
+        />
+      </Box>
+    </>
+  );
 }
